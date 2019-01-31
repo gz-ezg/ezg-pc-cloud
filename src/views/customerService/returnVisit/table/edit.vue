c6bf58f638cf0fbe2deb256839806ab6e18dc95e
fengzf <779640403@qq.com>
2019-01-31 11:26:16 +0800

    test

--- src/views/customerService/returnVisit/table/edit.vue
+++ src/views/customerService/returnVisit/table/edit.vue
@@ -67,6 +67,25 @@
                         </Col>
                     </Row>
                     <Row :gutter="16">
+                        <Col span="12">
+                            <FormItem prop="reply_time" label="答复时间:">
+                                <Rate allow-half v-model="data.reply_time"></Rate>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem prop="professional_level" label="专业程度:">
+                                <Rate allow-half v-model="data.professional_level"></Rate>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row :gutter="16">
+                        <Col span="12">
+                            <FormItem prop="service_attitude" label="服务态度:">
+                                <Rate allow-half v-model="data.service_attitude"></Rate>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row :gutter="16">
                         <Col span="24">
                             <FormItem prop="depart" label="责任部门：">
                                 <CheckboxGroup v-model="data.depart">
