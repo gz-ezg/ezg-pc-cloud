ffeac812581025420d1c280c85005b04a474d134
yangruilong2019 <173447104@qq.com>
2019-04-24 14:52:27 +0800

    新增注销工单，修改服务管理页面问题，增加服务管理搜索功能

--- src/views/productManagement/productListNew/updateCycleProperty.vue
+++ src/views/productManagement/productListNew/updateCycleProperty.vue
@@ -3,19 +3,19 @@
 		<Modal title="修改周期性属性" :value="true" width="600" @on-cancel="close">
 			<Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="120">
 				<FormItem label="A类外勤数量" prop="typeACount">
-					<InputNumber :min="1" v-model="formValidate.typeACount"></InputNumber>
+					<InputNumber :min="0" v-model="formValidate.typeACount"></InputNumber>
 					<!-- <Input v-model="formValidate.typeACount" placeholder="Enter your name"></Input> -->
 				</FormItem>
 				<FormItem label="B类外勤数量" prop="typeBCount">
-					<InputNumber :min="1" v-model="formValidate.typeBCount"></InputNumber>
+					<InputNumber :min="0" v-model="formValidate.typeBCount"></InputNumber>
 					<!-- <Input v-model="formValidate.typeBCount" placeholder="Enter your name"></Input> -->
 				</FormItem>
 				<FormItem label="产品份数" prop="portionNum">
-					<InputNumber :min="1" v-model="formValidate.portionNum"></InputNumber>
+					<InputNumber :min="0" v-model="formValidate.portionNum"></InputNumber>
 					<!-- <Input v-model="formValidate.portionNum" placeholder="Enter your name"></Input> -->
 				</FormItem>
 				<FormItem label="产品默认展示份数" prop="showPortionNum">
-					<InputNumber :min="1" v-model="formValidate.showPortionNum"></InputNumber>
+					<InputNumber :min="0" v-model="formValidate.showPortionNum"></InputNumber>
 					<!-- <Input v-model="formValidate.showPortionNum" placeholder="Enter your name"></Input> -->
 				</FormItem>
 				<FormItem label="最大票据数" prop="voucherMaxNum">
