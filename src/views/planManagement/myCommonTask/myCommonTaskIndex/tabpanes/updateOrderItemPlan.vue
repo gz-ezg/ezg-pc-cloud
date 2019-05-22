8b9f4f816481cf2ef6725d802e9c1335dfc1f20c
yangruilong2019 <173447104@qq.com>
2019-05-22 16:24:48 +0800

    新增注销工单，修改服务管理页面问题，增加服务管理搜索功能

--- src/views/planManagement/myCommonTask/myCommonTaskIndex/tabpanes/updateOrderItemPlan.vue
+++ src/views/planManagement/myCommonTask/myCommonTaskIndex/tabpanes/updateOrderItemPlan.vue
@@ -8,10 +8,11 @@
     >
         <Form ref="formItem"   :label-width="100" style="width:300px;margin:auto">
             <Row :gutter="16">
-                <FormItem prop="finish_status" label="申报年份：">
+                <FormItem label="申报年份：">
                     <DatePicker
                             type="year"
                             format="yyyy"
+                            v-model="formItem.declare_year"
                             :disabled="isDisabled"
                             placeholder="选择年份"
                             style="width: 120px"
@@ -20,32 +21,32 @@
                 </Col>
             </Row>
             <Row>
-                <FormItem prop="finish_status" label="收款类型：">
-                    <RadioGroup  v-model='monthServerChangeFlag' v-on >
-                        <Radio label="定额收款">定额收款</Radio>
-                        <Radio label="比例收款">比例收款</Radio>
+                <FormItem label="收款类型：">
+                    <RadioGroup  v-model='formItem.receipt_flag' v-on >
+                        <Radio label="quota">定额收款</Radio>
+                        <Radio label="proportion">比例收款</Radio>
                     </RadioGroup>
                 </FormItem>
 
             </Row>
 
-            <Row :gutter="16">
+            <Row :gutter="16" v-if=" formItem.receipt_flag == 'quota'">
                 <Col span="24">
-                <FormItem prop="record" label="定额收款：">
+                <FormItem label="定额收款：">
                     <Input type="text" v-model="formItem.company_amount" :rows=rows></Input>
                 </FormItem>
                 </Col>
             </Row>
-            <Row>
-                <FormItem  prop="departid"  label="收款比例：">
-                    <Input type="text" v-model="formItem.receipt_proportion" :rows=rows></Input>
+            <Row :gutter="16" >
+                <FormItem  v-if=" formItem.receipt_flag == 'proportion'"  label="收款比例：">
+                    <Input type="text" v-model="formItem.receipt_proportion" :rows=rows></Input>%
                 </FormItem>
 
             </Row>
 
         </Form>
         <div slot="footer">
-            <Button type="primary"  :loading="loading">确认</Button>
+            <Button type="primary"  :loading="loading" @click="updateOrderItemPlan">确认</Button>
         </div>
 
     </Modal>
@@ -60,17 +61,48 @@
 
             return{
                 loading: false,
+                currentRow:{},
                 //  新增数据
                 formItem:{},
                 openUpdateOrderItemPlan:false
             }
         },
         methods:{
+
+            updateOrderItemPlan(){
+                let _self = this
+                if(!_self.formItem.declare_year ){
+                    _self.$Message.warning("请填写申报年份");
+                }
+                console.log(_self.formItem.declare_year);
+
+                let url = 'api/order/item/plan/update'
+                let _data = {
+                    itemId: _self.currentRow.itemId,
+                    declareYear:_self.formItem.declare_year.getFullYear(),
+                    receiptType:_self.formItem.receipt_flag,
+                    paynumber: _self.formItem.company_amount,
+                    receiptProportion:_self.formItem.receipt_proportion
+                }
+
+                function doSuccess(res) {
+                    console.log(res);
+
+                }
+
+                function fail(err){
+
+
+                }
+
+                _self.$Post(url, _data, doSuccess)
+            }
         },
         created(){
             let _self =this;
             this.$bus.on("update_order_item_plan",(e)=>{
                 _self.openUpdateOrderItemPlan = true
+            _self.currentRow = e;
         })
         }
     }
