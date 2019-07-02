bbbf8cc6332681e4f686207ee67375477215ec44
lyc <893608341@qq.com>
2019-07-02 15:07:26 +0800

    xiaoxiaoxiao

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ set -e
 git add -A
 git commit -m "$1"
 git push origin master
-exit
-
 echo '正在构建'
 curl -X POST "build.yrl.fun/job/front_cloud_zgcfo_front/build?token=yjg" --user admin:build.123
 echo '部署成功'
