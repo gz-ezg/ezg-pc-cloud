a8f9e3dee2ec4cf017cbb95f5509fb60602b925b
lyc <893608341@qq.com>
2019-07-02 14:38:14 +0800

    "测试"

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env sh
 
 set -e
+ echo $1
 git add -A
 git commit -m "$1"
-git push origin master 
+git push origin master
+
+echo '正在构建'
 curl -X POST "build.yrl.fun/job/front_cloud_zgcfo_front/build?token=yjg" --user admin:build.123
 echo '部署成功'
-echo '正在构建'
-npm run build
-rm -rf yjg
-mkdir yjg
-cd ./dist
-tar -czf dist.tar.gz *
-mv dist.tar.gz  ../yjg/dist.tar.gz
-echo '部署成功'
+
