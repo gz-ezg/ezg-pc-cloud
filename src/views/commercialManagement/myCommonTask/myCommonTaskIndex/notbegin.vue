c786c76de5188054d32204d83ad69c06e6d7ed96
Jeffy <925800148@qq.com>
2020-06-01 19:34:23 +0800

    add chenzc

--- src/views/commercialManagement/myCommonTask/myCommonTaskIndex/notbegin.vue
+++ src/views/commercialManagement/myCommonTask/myCommonTaskIndex/notbegin.vue
@@ -874,10 +874,7 @@ export default {
     Bus.$on("flowsuccess", e => {
       _self.getData();
     });
-    if (
-      localStorage.getItem("id") == 10228 ||
-      localStorage.getItem("id") == 10059
-    ) {
+    if (localStorage.getItem("id") == 10059) {
       _self.isAdmin = true;
     } else {
       _self.isAdmin = false;
